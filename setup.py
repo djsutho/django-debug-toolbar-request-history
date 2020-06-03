8b94c802f8193fde1b9c6778f481759d7b7aae1c
David Sutherland <djsutho@gmail.com>
2020-06-03 22:54:07 +1000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-debug-toolbar-request-history',
-    version='0.1.1',
+    version='0.1.2',
     description='Request History Panel for Django Debug Toolbar',
     author='David Sutherland',
     author_email='djsutho@gmail.com',
@@ -11,7 +11,7 @@ setup(
     include_package_data=True,
     install_requires=['django-debug-toolbar>=2.0'],
     url='https://github.com/djsutho/django-debug-toolbar-request-history',
-    download_url='https://github.com/djsutho/django-debug-toolbar-request-history/tarball/0.1.1',
+    download_url='https://github.com/djsutho/django-debug-toolbar-request-history/tarball/0.1.2',
     keywords=['django', 'debug', 'ajax'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
